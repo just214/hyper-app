5688975551ce8f6d3bdd98f686239f09d4d0e76b
gojutin <gojutin@gmail.com>
2021-02-08 17:46:08 -0500

    Added change-case to utils

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,17 @@
 export function padNumber(number: number) {
   return number.toString().padStart(2, "0");
 }
+
+export {
+  camelCase,
+  capitalCase,
+  constantCase,
+  dotCase,
+  headerCase,
+  noCase,
+  paramCase,
+  pascalCase,
+  pathCase,
+  sentenceCase,
+  snakeCase,
+} from "change-case";
